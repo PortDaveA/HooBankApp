1fea1dd2d6ed128151f695b7d595cffac3b555a7
DaveA <91314082+PortDaveA@users.noreply.github.com>
2022-12-07 14:14:08 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/hooBankApp/', 
+  base: "https://portdavea.github.io/hooBankApp/", 
   plugins: [react()]
 })
