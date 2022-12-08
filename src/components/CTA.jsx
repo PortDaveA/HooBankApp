34f5c284f227e167d841822ec259d2ee070bc09b
DaveA <91314082+PortDaveA@users.noreply.github.com>
2022-12-08 16:37:11 +0500

    Update CTA.jsx

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from '../style';
-import Button from './Button';
+import Button from './Button.jsx';
 
 const CTA = () => (
   <section className = {`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
