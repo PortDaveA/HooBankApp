5dd58389db5641eee26dcb37c461baf1f41dddea
DaveA <91314082+PortDaveA@users.noreply.github.com>
2022-12-08 16:38:37 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../style';
 import {discount, robot} from '../assets';
-import GetStarted from './GetStarted';
+import GetStarted from './GetStarted.jsx';
 
 
 const Hero = () => (
