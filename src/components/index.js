0d5b187a5df9c708def238dba256510881b791a1
DaveA <91314082+PortDaveA@users.noreply.github.com>
2022-12-09 10:15:45 +0500

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,13 +1,13 @@
-import {NavBar} from "./NavBar.jsx";
-import {CardDeal} from "./CardDeal.jsx";
-import {Business} from "./Business.jsx";
-import {Clients} from "./Clients.jsx";
-import {CTA} from "./CTA.jsx";
-import {Stats} from "./Stats.jsx";
-import {Footer} from "./Footer.jsx";
-import {Testimonials} from "./Testimonials.jsx";
-import {Hero} from "./Hero.jsx";
-import {Billing} from "./Billing.jsx";
+import NavBar from "./NavBar.jsx";
+import CardDeal from "./CardDeal.jsx";
+import Business from "./Business.jsx";
+import Clients from "./Clients.jsx";
+import CTA from "./CTA.jsx";
+import Stats from "./Stats.jsx";
+import Footer from "./Footer.jsx";
+import Testimonials from "./Testimonials.jsx";
+import Hero from "./Hero.jsx";
+import Billing from "./Billing.jsx";
 
 
 export{
